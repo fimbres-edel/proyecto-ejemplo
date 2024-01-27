a6323975323d90e965669137b612e7210a187b32
fimbres-edel <fimbres.edel@gmail.com>
2024-01-27 12:31:19 -0600

    actualizacion 12724

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hola Mundo
+          Actualizacion para git
         </p>
         <a
           className="App-link"
